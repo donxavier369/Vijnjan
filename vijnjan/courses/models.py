325e0a717b12a586de1e0e1077a5f795a990fede
Don Xavier <donxavier369@gmail.com>
2024-04-01 16:34:58 +0530

    add thumbnail field in courses model

--- vijnjan/courses/models.py
+++ vijnjan/courses/models.py
@@ -1,9 +1,20 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from PIL import Image
 # Create your models here.
+
+def validate_thumbnail_size(value):
+    # Opening the image
+    img = Image.open(value)
+
+    # Checking the file size
+    if value.size > 20 * 1024:  # 20KB
+        raise ValidationError("Image file size cannot exceed 20KB.")
 
 class Courses(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=255, default="description")
+    thumbnail = models.ImageField(upload_to='courses/thumbnail', validators=[validate_thumbnail_size])
     duration = models.IntegerField(default=1)
     tutor = models.ForeignKey('accounts.CustomUser', on_delete=models.CASCADE)
     is_trending = models.BooleanField(default=False)
@@ -15,7 +26,7 @@ class Modules(models.Model):
     module_name = models.CharField(max_length=50)
     module_type = models.CharField(max_length=50)
     module_content_ppt = models.FileField(upload_to='modules/ppt', null=True, blank=True)
-    module_content_video = models.FileField(upload_to='modules/video', null=True, blank=True)
+    module_content_video = models.FileField(upload_to='modules/video')
     
 
 
--- vijnjan/courses/serializers.py
+++ vijnjan/courses/serializers.py
@@ -15,7 +15,7 @@ class CourseSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Courses
-        fields = ('id', 'name', 'description', 'duration', 'tutor')
+        fields = ('id', 'name', 'description', 'thumbnail', 'duration', 'tutor')
 
         # Validate duration to be positive
         extra_kwargs = {'duration': {'validators': [MinValueValidator(1)]}}
