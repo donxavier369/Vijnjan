ded596ca53b9224cb542225c02bd2d5792bfa24d
Don Xavier <donxavier369@gmail.com>
2024-04-24 22:49:41 +0530

    remove cloudinary from settings.py installed apps

--- vijnjan/vijnjan/settings.py
+++ vijnjan/vijnjan/settings.py
@@ -46,8 +46,7 @@ INSTALLED_APPS = [
     # 'rest_framework.authtoken',
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
-    'cloudinary',
-    'cloudinary_storage',
+    
     # 'corsheaders',
     'accounts',
     'meetings',
