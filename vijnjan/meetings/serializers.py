017cc823df4c07c6cb42e19e41c821e1952d8e4f
Don Xavier <donxavier369@gmail.com>
2024-05-06 22:51:38 +0530

    Add aditional field am_pm in meeting model

--- vijnjan/meetings/serializers.py
+++ vijnjan/meetings/serializers.py
@@ -8,5 +8,5 @@ class MeetingSerializer(serializers.ModelSerializer):
     tutor = serializers.PrimaryKeyRelatedField(queryset=CustomUser.objects.filter(person = 'tutor'))
     class Meta:
         model = Meetings
-        fields = ['id', 'date', 'time', 'course', 'duration', 'tutor', 'link']
+        fields = ['id', 'date', 'time', 'am_pm', 'course', 'duration', 'tutor', 'link']
 
