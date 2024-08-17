1ed3835b09b0693446f0b02a31c2573c99e4035f
Don Xavier <donxavier369@gmail.com>
2024-08-17 14:53:34 +0530

    add welcome page

--- vijnjan/vijnjan/urls.py
+++ vijnjan/vijnjan/urls.py
@@ -18,11 +18,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import home_view
 
 
 
 
 urlpatterns = [
+    path('', home_view, name='home'),
     path('admin/', admin.site.urls),
     path('user/', include('accounts.urls')),
     path('meetings/', include('meetings.urls')),
